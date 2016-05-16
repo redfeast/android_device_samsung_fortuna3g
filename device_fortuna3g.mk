b160d28d1c09d29f333027708f873615137cc0cd
ZIM <zim555@mail.ru>
2016-05-16 11:13:11 +0300

    Update device_fortuna3g.mk

--- device_fortuna3g.mk
+++ device_fortuna3g.mk
@@ -38,6 +38,7 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/rootdir/init.qcom.rc:root/init.qcom.rc \
 	$(LOCAL_PATH)/rootdir/init.target.rc:root/init.target.rc \
 	$(LOCAL_PATH)/rootdir/init.qcom.usb.rc:root/init.qcom.usb.rc \
+	$(LOCAL_PATH)/rootdir/init.qcom.power.rc:root/init.qcom.power.rc \
 	$(LOCAL_PATH)/rootdir/init.qcom.factory.sh:root/init.qcom.factory.sh \
 	$(LOCAL_PATH)/rootdir/init.class_main.sh:root/init.class_main.sh \
 	$(LOCAL_PATH)/rootdir/init.mdm.sh:root/init.mdm.sh \
