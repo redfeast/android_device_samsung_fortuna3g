aae16893bd9e409bffff88e896a2c3e1dce42982
ZIM <zim555@mail.ru>
2016-05-23 21:31:47 +0300

    Update device_fortuna3g.mk

--- device_fortuna3g.mk
+++ device_fortuna3g.mk
@@ -64,7 +64,6 @@ PRODUCT_COPY_FILES += \
 	$(LOCAL_PATH)/keylayout/synaptics_rmi4_i2c.kl:system/usr/keylayout/synaptics_rmi4_i2c.kl
 	
 # Configurations
-# $(LOCAL_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf
 # $(LOCAL_PATH)/configs/audio_platform_info.xml:system/etc/audio_platform_info.xml
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
@@ -72,6 +71,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/audio_effects.conf:system/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/configs/mixer_paths.xml:system/etc/mixer_paths.xml \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config \
